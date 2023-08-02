c712ed3a73f8d3f4eda2bd90910cdba02d65c1b1
Нурдаулет Хожакельдиев <khozhakeldiyev_n@bi.group>
2023-08-03 02:35:19 +0600

    feat: create blank accountController;

---
+++ AuthAPI/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class AccountController : ControllerBase
+    {
+        public AccountController()
+        {
+
+        }
+
+        public async Task<IActionResult> SignUp()
+        {
+            return Ok(); 
+        }
+
+        public async Task<IActionResult> SignIn()
+        {
+            return Ok();
+        }
+    }
+}
